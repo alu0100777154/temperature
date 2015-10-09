eec8b8572cb15135ec19264f737aea34d425d520
Guille <Guille@DESKTOP-6SLHS3L>
2015-10-09 09:05:28 +0100

    expresion

--- public_html/temperature.js
+++ public_html/temperature.js
@@ -3,7 +3,7 @@ function calculate() {
   var result;
   var original       = document.getElementById("original");
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcCkK])/;
+  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
   
   var m = temp.match(regexp);
   
